8743ad719bd4fbeb4f6ee408f22ca7fb56e5a4d3
Nathan Camarena <59515478+ncamarena57@users.noreply.github.com>
2024-01-16 10:04:29 -0800

    Add event listener for submit button

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function submit() {
     alert("Hello world");
 }
+
+const submitBtn = document.querySelector(".submit");
+
+submitBtn.addEventListener("click", function() {
+    console.log("You have submitted a new task.");
+});
