4899f14af32f4c1066d0aa78f8f862e78a1d913b
Nathan Camarena <59515478+ncamarena57@users.noreply.github.com>
2024-01-16 10:23:15 -0800

    Add list item on click and reset input field

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ function submit() {
 const submitBtn = document.querySelector(".submit");
 
 submitBtn.addEventListener("click", function() {
-    console.log("You have submitted a new task.");
+    //console.log("You have submitted a new task.");
+    const input = document.getElementById("new-task").value;
+    const li = document.createElement("li");
+    li.innerText = input;
+    const ul = document.querySelector(".list");
+    ul.appendChild(li);
+    resetInput();
 });
+
+function resetInput() {
+    document.getElementById("new-task").value = "";
+}
